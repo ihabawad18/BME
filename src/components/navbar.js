0e75d6de9255d7c72a48588e8dbbad03a7c2776f
Ihab Awad <ihabawad767@Gmail.com>
2023-12-31 02:42:48 +0200

    adding responsiveness to home page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,36 @@
-import React from 'react'
-import whiteLogo from '../images/white-logo-02.png';
-import './navbar.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import whiteLogo from "../images/white-logo-02.png";
+import { Link } from "react-router-dom";
+import "./navbar.css";
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
     <div className="nav">
-        <img src={whiteLogo} alt='logo'/>
-        <ul className='nav-links'>
-            <li><Link to='/about-us'>About Us</Link></li>
-            <li><Link to='/services'>Services</Link></li>
-            <li><Link to='/why-bme'>Why BME?</Link></li>
-            <li><Link to='/contact-us'>Contact Us</Link></li>
-        </ul>
+      <img src={whiteLogo} alt="logo" />
+      <div className="burger-icon" onClick={toggleLinks}>
+        <i className={`fas ${showLinks ? "fa-times" : "fa-bars"}`}></i>
+      </div>
+      <ul className={`nav-links ${showLinks ? "show" : ""}`}>
+        <li>
+          <Link to="/about-us">About Us</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/why-bme">Why BME?</Link>
+        </li>
+        <li>
+          <Link to="/contact-us">Contact Us</Link>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
